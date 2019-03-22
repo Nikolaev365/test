7055110a4081eaf7fc5db519fbe2a3e89c2c1a79
Ivan <webexp365@gmail.com>
2019-03-22 13:10:13 +0300

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,13 @@
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+// console.log(persone['name']);
+// let arr = ['plum.png', 'or.jpg', 'apple.bmp'];
+// console.log(arr[2]);
+// alert("Hellow");
+// let anner = confirm("Are you?");
+// console.log(anner);
+let anner = prompt("Есть ли вам 18?", "Да", "");
+console.log(typeof(anner));
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+let anner = prompt("Есть ли вам 18?", "Да", "");
+console.log(typeof(anner));
